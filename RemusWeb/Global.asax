239760c063a81b52d982153c03eba5026d95745e
Tyler Shaw <tyler.shaw@acaexpress.local>
2016-12-13 10:54:43 -0600

    init commit

---
+++ RemusWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RemusWeb.MvcApplication" Language="C#" %>
---
+++ RemusWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RemusWeb.Startup))]
+namespace RemusWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
